cba71d3675b04fb724a5a3fd52a851d4f9f5b989
Insu Jang <insujang@casys.kaist.ac.kr>
2021-04-15 13:26:51 +0000

    Add /dev/ioat-dma creation

--- ioat-dma.c
+++ ioat-dma.c
@@ -1,15 +1,98 @@
+/**
+ * @file ioat-dma.c
+ * @author Insu Jang (insujang@casys.kaist.ac.kr)
+ * @brief Kernel module for DMA operations with an I/OAT DMA engine.
+ * @version 0.1
+ * @date 2021-04-15
+ * 
+ * Reference articles
+ * https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html
+ * https://forums.xilinx.com/xlnx/attachments/xlnx/ELINUX/31239/1/axidma.c
+ */
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
 
-static int __init ioat_dma_init(void) {
+static int ioat_dma_open(struct inode *, struct file *);
+static int ioat_dma_release(struct inode *, struct file *);
+
+static struct file_operations ioat_dma_fops = {
+  .open = ioat_dma_open,
+  .release = ioat_dma_release,
+};
+
+
+static int ioat_dma_open(struct inode *inode, struct file *file) {
   printk(KERN_INFO "%s\n", __func__);
+  return 0;
+}
+
+static int ioat_dma_release(struct inode *inode, struct file *file) {
+  printk(KERN_INFO "%s\n", __func__);
+  return 0;
+}
+
+
+static dev_t dev;
+static struct cdev cdev;
+static struct class *pClass;
+static struct device *pDev;
+#define DEVICE_NAME "ioat-dma"
+#define MAX_MINORS 5
+
+/**
+ * https://topic.alibabacloud.com/a/the-register_chrdev-and-register_chrdev_region-of-character-devices_8_8_31256510.html
+ * Difference between register_chrdev and register_chrdev_region
+ */
+static int __init ioat_dma_init(void) {
+  int ret;
+  printk(KERN_INFO "%s\n", __func__);
+
+  // Register character device
+  ret = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
+  if (ret < 0) {
+    printk (KERN_ALERT "%s: alloc_chrdev_region failed with %d.\n", __func__, ret);
+    return ret;
+  }
+
+  ret = register_chrdev_region(dev, 0, DEVICE_NAME);
+  if (ret < 0) {
+    printk (KERN_ALERT "%s: register_chrdev_region failed with %d.\n", __func__, ret);
+    return ret;
+  }
+
+  cdev_init(&cdev, &ioat_dma_fops);
+  cdev_add(&cdev, dev, 1);
+
+  // Create /sys/class/ioat-dma in preparation of creating /dev/ioat-dma
+  pClass = class_create(THIS_MODULE, DEVICE_NAME);
+  if (IS_ERR(pClass)) {
+    printk (KERN_ALERT "%s: class_create failed.\n", __func__);
+    unregister_chrdev_region(dev, 1);
+    return -1;
+  }
+
+  // Create /dev/ioat-dma
+  pDev = device_create(pClass, NULL, dev, NULL, DEVICE_NAME);
+  if (IS_ERR(pDev)) {
+    printk(KERN_ALERT "%s: device_create failed.\n", __func__);
+    class_destroy(pClass);
+    unregister_chrdev_region(dev, 1);
+    return -1;
+  }
+
   return 0;
 }
 
 static void __exit ioat_dma_exit(void) {
   printk(KERN_INFO "%s\n", __func__);
+  device_destroy(pClass, dev);
+  class_destroy(pClass);
+  cdev_del(&cdev);
+  unregister_chrdev_region(dev, 1);
 }
 
 MODULE_AUTHOR("Insu Jang");
