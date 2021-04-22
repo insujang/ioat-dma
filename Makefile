20b5cce0ba49ca578af6461041cee34227d46935
Insu Jang <insujang@casys.kaist.ac.kr>
2021-04-22 13:32:28 +0000

    Add dma vs memcpy test code (C)

--- Makefile
+++ Makefile
@@ -5,6 +5,8 @@ device_dax-y := device.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	$(CC) test.c -o test
 
 clean:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	rm -f test
---
+++ test.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
+#include <sys/time.h>
+
+/**
+ * @brief This is a test code that compares performance between
+ * DMA and manual memcpy().
+ */
+
+static unsigned long long src_offset = 0x0;
+static unsigned long long dst_offset = 0x10;
+static unsigned long long size = 0x2000000;
+
+static char *generate_random_bytestream(size_t num_bytes) {
+  char *stream = malloc(num_bytes);
+  for (size_t i=0; i<num_bytes; i++) {
+    stream[i] = rand();
+  }
+  return stream;
+}
+
+static int check_same_bytestream(const char *a, const char *b, size_t num_bytes) {
+  for (size_t i=0; i<num_bytes; i++) {
+    if (a[i] != b[i])
+      return -1;
+  }
+  return 0;
+}
+
+double perform_dma(int ioat_fd, void *src, void *dst);
+double perform_memcpy(void *src, void *dst);
+
+int main() {
+  int dax_fd = open("/dev/dax0.0", O_RDWR);
+  int ioat_fd = open("/dev/ioat-dma", O_RDWR);
+
+  if (dax_fd < 0 || ioat_fd < 0) {
+    printf("Failed to open device\n");
+    return -1;
+  }
+
+  void *src = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, dax_fd, src_offset * size);
+  void *dst = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, dax_fd, dst_offset * size);
+  if (!src || !dst) {
+    printf("Failed to mmap\n");
+    return -1;
+  }
+
+  double dma_time = perform_dma(ioat_fd, src, dst);
+  double memcpy_time = perform_memcpy(src, dst);
+
+  printf("DMA: %lf s, memcpy: %lf s\n", dma_time, memcpy_time);
+}
+
+struct ioctl_dma_args {
+  char device_name[64];
+  uint64_t src_offset;
+  uint64_t dst_offset;
+  uint64_t size;
+} __attribute__ ((packed));
+
+#define IOCTL_IOAT_DMA_SUBMIT _IOW(0xad, 0, struct ioctl_dma_args)
+double perform_dma(int ioat_fd, void *src, void *dst) {
+  struct timeval start, end;
+  char *data = data = generate_random_bytestream(size);
+
+  memcpy(src, data, size);
+  check_same_bytestream(src, data, size);
+  free(data);
+
+  struct ioctl_dma_args args = {
+    .device_name = "/dev/dax0.0",
+    .src_offset = src_offset * size,
+    .dst_offset = dst_offset * size,
+    .size = size,
+  };
+  gettimeofday(&start, 0);
+  int result = ioctl(ioat_fd, IOCTL_IOAT_DMA_SUBMIT, &args);
+  gettimeofday(&end, 0);
+
+  return (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
+}
+
+double perform_memcpy(void *src, void *dst) {
+  struct timeval start, end;
+  char *data = data = generate_random_bytestream(size);
+
+  memcpy(src, data, size);
+  check_same_bytestream(src, data, size);
+  free(data);
+
+  gettimeofday(&start, 0);
+  memcpy(dst, src, size);
+  gettimeofday(&end, 0);
+
+  return (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
+}
