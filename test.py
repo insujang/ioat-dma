0ce4614a79d4b5f9cdc4fb961292d16146ae5aca
Insu Jang <insujang@casys.kaist.ac.kr>
2021-04-22 11:32:12 +0000

    Clean test code

--- test.py
+++ test.py
@@ -6,17 +6,20 @@ from ioctl_numbers import _IOW
 
 class TestIoatDma(unittest.TestCase):
     def setUp(self):
-        self.fd = os.open("/dev/ioat-dma", os.O_RDWR)
-        self.assertGreater(self.fd, 0)
+        self.dax = os.open("/dev/dax0.0", os.O_RDWR)
+        self.assertGreater(self.dax, 0)
+        self.ioat = os.open("/dev/ioat-dma", os.O_RDWR)
+        self.assertGreater(self.ioat, 0)
 
     def tearDown(self):
-        os.close(self.fd)
+        os.close(self.dax)
+        os.close(self.ioat)
 
     def test_nothing(self):
         self.assertEqual(0, 0)
 
     # @unittest.skip
-    def test_ioctl(self):
+    def test_ioat_dma(self):
         # 64sQQQ format (https://docs.python.org/3/library/struct.html#format-characters)
         # 64 bytes char[], 3 consecutive unsigned long longs, int
         # Equivalent to:
@@ -32,15 +35,11 @@ class TestIoatDma(unittest.TestCase):
                             0x1000,
                             0x500)
         try:
-            fcntl.ioctl(self.fd, _IOW(0xad, 0, 88), arg)
+            fcntl.ioctl(self.ioat, _IOW(0xad, 0, 88), arg)
         except OSError as err:
             self.fail("ioctl() returns an error: {}".format(err))
         
 
 
 if __name__ == '__main__':
-    # open dax device to prevent kernel NULLptr dereference
-    fd = os.open("/dev/dax0.0", os.O_RDWR)
-    os.close(fd)
-
     unittest.main()
